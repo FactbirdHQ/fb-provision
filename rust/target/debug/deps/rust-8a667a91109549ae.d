5d2e338ae8fe0e0b6a398a965e45db9be03b2615
Mathias <mk@blackbird.online>
2023-02-24 15:09:52 +0100

    Add gitignore files

--- rust/target/debug/deps/rust-8a667a91109549ae.d
+++
@@ -1,5 +0,0 @@
-/home/mathias/git/blackbird/bb-provision/rust/target/debug/deps/rust-8a667a91109549ae.rmeta: src/main.rs
-
-/home/mathias/git/blackbird/bb-provision/rust/target/debug/deps/rust-8a667a91109549ae.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/deps/rust-fc6dd6ea4296fa5b.d
+++
@@ -1,5 +0,0 @@
-/home/mathias/git/blackbird/bb-provision/rust/target/debug/deps/rust-fc6dd6ea4296fa5b.rmeta: src/main.rs
-
-/home/mathias/git/blackbird/bb-provision/rust/target/debug/deps/rust-fc6dd6ea4296fa5b.d: src/main.rs
-
-src/main.rs:
